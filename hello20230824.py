4138427fb1f39379689b768603bb3d2630951de1
HYYU <hyyu713@gmail.com>
2023-08-24 20:54:45 +0800

    add new file

---
+++ hello20230824.py
@@ -0,0 +1,11 @@
+p = 50
+h = 50.0
+red = True
+name = 'yu'
+print(p, h, red, name)
+print(type(p))
+print(type(h))
+print(type(red))
+print(type(name))
+name = input('your name: ')
+print('Hello!', name )
